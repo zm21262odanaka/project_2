4a6a4fb50c5f44ca1b1c9ddd15050efdf3b6cca0
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-02-07 11:19:32 +0900

    Update kibun.js

--- kibun.js
+++ kibun.js
@@ -2,8 +2,8 @@ function kibun() {
 if (document.getElementById('answer').value=='明るい') {
  alert('フレデリックのオンリーワンダー');
 }
-function nazo5() {
+else {
 if (document.getElementById('answer').value=='静かめ') {
- alert('official　髭男 dismの115万キロのフィルム');
+ alert ('official 髭男 dismの115万キロのフィルム');
+ }
 }
-
