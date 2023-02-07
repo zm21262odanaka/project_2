303ce6892c5c21f75dfc60c06c78a49f08bd5f88
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-02-07 16:47:35 +0900

    Delete kibun.js

--- kibun.js
+++
@@ -1,9 +0,0 @@
-function kibun() {
-if (document.getElementById('answer').value=='明るい') {
- alert('フレデリックのオンリーワンダー');
-}
-else 
- if (document.getElementById('answer').value=='静かめ') {
- alert ('official 髭男 dismの115万キロのフィルム');
- }
-} 
